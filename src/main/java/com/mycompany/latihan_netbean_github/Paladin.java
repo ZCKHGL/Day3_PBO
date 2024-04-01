dab09712b457710df72817d766a267a308184a9a
A-12 <A-12@A-12>
2024-04-01 10:41:48 +0700

    inisialisasi awal

---
+++ src/main/java/com/mycompany/latihan_netbean_github/Paladin.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.latihan_netbean_github;
+
+/**
+ *
+ * @author A-12
+ */
+public class Paladin {
+    public static void main(String[] args) {
+        System.out.println("Hawoo!");
+        
+    }
+}
